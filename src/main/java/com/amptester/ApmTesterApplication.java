73ac32ff8e310315cd7cc512e15aafc8c61f72ae
冀刚 <jiweigang2008@126.com>
2022-03-01 06:16:26 +0000

    index

--- src/main/java/com/amptester/ApmTesterApplication.java
+++ src/main/java/com/amptester/ApmTesterApplication.java
@@ -2,13 +2,9 @@ package com.amptester;
 
 import com.amptester.spider.Spider;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
-@ServletComponentScan
 public class ApmTesterApplication {
-    public static void main(String[] args) {
+    public static void run(String[] args) {
         SpringApplication application = new SpringApplication(ApmTesterApplication.class);
         application.run(args);
         new Spider().run();
